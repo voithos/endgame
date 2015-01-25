dec109d2b3afc0d9a0346408d55a9fc684110c35
Zaven Muradyan <zaven@skepsi.me>
2015-01-24 19:50:16 -0800

    Add 'waiting' dialog

--- src/views.js
+++ src/views.js
@@ -5,7 +5,25 @@ var log = require('./log');
 
 module.exports = {
     showWaitScreen: function(gameId) {
-        log('the game is', gameId);
+        log('the gameid is', gameId);
+        var self = this;
+
+        var url = window.location.protocol + '//' + window.location.hostname +
+            (window.location.port ? ':' + window.location.port : '');
+        var gameUrl = url + '/' + gameId;
+
+        self.waitScreen = new Vue({
+            el: '#waitscreen',
+            data: {
+                link: gameUrl
+            }
+        });
+
+        $('#waitscreen').modal({
+            backdrop: false,
+            keyboard: false
+        });
+
         return Promise.resolve();
     }
 };
